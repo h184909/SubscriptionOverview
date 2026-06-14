9d307f9d25dcb3b07eeca1386c5db88639a64136
Bjarte Sætre <184909@stud.hvl.no>
2026-06-14 15:59:33 +0200

    dsa

--- src/main/java/no/hvl/subscriptionapp/repository/BankConsentRepository.java
+++ src/main/java/no/hvl/subscriptionapp/repository/BankConsentRepository.java
@@ -2,13 +2,17 @@ package no.hvl.subscriptionapp.repository;
 
 import no.hvl.subscriptionapp.domain.BankConsent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface BankConsentRepository extends JpaRepository<BankConsent, UUID> {
+
     Optional<BankConsent> findTopByUserEmailOrderByCreatedAtDesc(String userEmail);
+
+    @Transactional
+    @Modifying
     void deleteByUserEmail(String userEmail);
-
-
 }
