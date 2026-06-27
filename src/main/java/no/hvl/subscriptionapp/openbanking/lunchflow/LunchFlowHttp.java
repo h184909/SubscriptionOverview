d8c98903353f0f85b8a936f6da501eccc84d9c89
Bjarte Sætre <184909@stud.hvl.no>
2026-06-28 01:18:08 +0200

    dsad

--- src/main/java/no/hvl/subscriptionapp/openbanking/lunchflow/LunchFlowHttp.java
+++ src/main/java/no/hvl/subscriptionapp/openbanking/lunchflow/LunchFlowHttp.java
@@ -9,15 +9,25 @@ import org.springframework.web.client.RestClient;
 public class LunchFlowHttp {
 
     private final RestClient client;
+    private final LunchFlowProperties props;
 
     public LunchFlowHttp(LunchFlowProperties props) {
+        this.props = props;
         this.client = RestClient.builder()
                 .baseUrl(props.getBaseUrl())
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                 .build();
+
+        System.out.println("================================");
+        System.out.println("LunchFlow baseUrl = " + props.getBaseUrl());
+        System.out.println("LunchFlow authorizeUrl = " + props.getAuthorizeUrl());
+        System.out.println("LunchFlow redirectUri = " + props.getRedirectUri());
+        System.out.println("================================");
     }
 
     public LunchFlowDtos.TokenResponse exchangeCode(LunchFlowDtos.TokenRequest body) {
+        System.out.println("LunchFlow POST " + props.getBaseUrl() + "/oauth/token");
+
         return client.post()
                 .uri("/oauth/token")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -27,6 +37,8 @@ public class LunchFlowHttp {
     }
 
     public LunchFlowDtos.AccountsResponse getAccounts(String accessToken) {
+        System.out.println("LunchFlow GET " + props.getBaseUrl() + "/accounts");
+
         return client.get()
                 .uri("/accounts")
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
@@ -38,6 +50,8 @@ public class LunchFlowHttp {
             String accessToken,
             String accountId
     ) {
+        System.out.println("LunchFlow GET " + props.getBaseUrl() + "/accounts/" + accountId + "/transactions");
+
         return client.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/accounts/{accountId}/transactions")
