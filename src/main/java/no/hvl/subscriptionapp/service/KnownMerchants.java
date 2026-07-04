23d7750bd3ed88930dc53526826091da643ef366
Bjarte Sætre <184909@stud.hvl.no>
2026-07-05 00:54:22 +0200

    dsa

--- src/main/java/no/hvl/subscriptionapp/service/KnownMerchants.java
+++ src/main/java/no/hvl/subscriptionapp/service/KnownMerchants.java
@@ -18,33 +18,42 @@ public final class KnownMerchants {
 
     private static final List<Rule> RULES = List.of(
             rule("netflix", ".*\\bnetflix\\b.*|.*netflix\\.com.*", "Netflix", "https://www.netflix.com/cancelplan", "Entertainment"),
-            rule("spotify", ".*\\bspotify\\b.*|.*spotifyab.*", "Spotify", "https://www.spotify.com/account/subscription/", "Music"),
+            rule("spotify", ".*\\bspotify\\b.*|.*spotifyab.*", "Spotify", "https://www.spotify.com/account/subscription/", "Entertainment"),
             rule("disney_plus", ".*\\bdisney\\s*plus\\b.*|.*\\bdisney\\+\\b.*|.*disneyplus.*", "Disney+", "https://www.disneyplus.com/account/subscription", "Entertainment"),
             rule("tv2_play", ".*\\btv\\s*2\\b.*|.*\\btv2\\b.*|.*tv2play.*|.*play\\.tv2.*", "TV 2 Play", "https://play.tv2.no/konto", "Entertainment"),
             rule("viaplay", ".*\\bvia\\s*play\\b.*|.*\\bviaplay\\b.*|.*viaplay\\.(com|no|se|dk).*|.*\\bviaplay\\s*group\\b.*", "Viaplay", "https://viaplay.com/no-no/account", "Entertainment"),
             rule("prime_video", ".*\\bprime\\s*video\\b.*|.*primevideo.*|.*\\bamazon\\s*prime\\b.*|.*amazon.*video.*", "Prime Video", "https://www.amazon.com/amazonprime", "Entertainment"),
             rule("youtube_premium", ".*\\byoutube\\b.*|.*paid_memberships.*", "YouTube Premium", "https://www.youtube.com/paid_memberships", "Entertainment"),
-
-            rule("apple_subscriptions", ".*apple\\.com/bill.*|.*itunes\\.com/bill.*|.*\\bitunes\\b.*|.*\\bicloud\\b.*|.*apple\\s*services.*", "Apple (Abonnement)", "https://apps.apple.com/account/subscriptions", "Cloud"),
-            rule("google_play", ".*google\\s*play.*|.*play\\.google\\.com.*|.*google\\s*payment.*", "Google Play (Abonnement)", "https://play.google.com/store/account/subscriptions", "Digital"),
-            rule("google_one", ".*\\bgoogle\\s*one\\b.*|.*google.*storage.*", "Google One", "https://one.google.com/", "Cloud"),
-            rule("chatgpt", ".*\\bopenai\\b.*|.*\\bchatgpt\\b.*", "ChatGPT", "https://chatgpt.com/", "AI"),
+            rule("storytel", ".*\\bstorytel\\b.*", "Storytel", "https://www.storytel.com/no/no/account", "Entertainment"),
+            rule("bookbeat", ".*\\bbookbeat\\b.*", "BookBeat", "https://www.bookbeat.com/no/account", "Entertainment"),
+            rule("fable", ".*\\bfable\\b.*", "Fable", "https://fable.no/kundeservice", "Entertainment"),
+            rule("audible", ".*\\baudible\\b.*", "Audible", "https://www.audible.com/account/cancel", "Entertainment"),
+            rule("rikstv", ".*\\briks\\s*tv\\b.*|.*\\brikstv\\b.*", "RiksTV", "https://www.rikstv.no/kundeservice/", "Entertainment"),
 
             rule("talkmore", ".*\\btalkmore\\b.*", "Talkmore", "https://www.talkmore.no/minside/", "Telecom"),
             rule("telenor", ".*\\btelenor\\b.*", "Telenor", "https://www.telenor.no/kundeservice/", "Telecom"),
             rule("telia", ".*\\btelia\\b.*", "Telia", "https://www.telia.no/kundeservice/", "Telecom"),
             rule("ice", ".*\\bice\\b.*", "Ice", "https://www.ice.no/kundeservice/", "Telecom"),
-            rule("altibox", ".*\\baltibox\\b.*|.*\\blyse\\b.*altibox.*", "Altibox", "https://www.altibox.no/privat/kundeservice/", "Utilities"),
-            rule("rikstv", ".*\\briks\\s*tv\\b.*|.*\\brikstv\\b.*", "RiksTV", "https://www.rikstv.no/kundeservice/", "Entertainment"),
-            rule("tibber", ".*\\btibber\\b.*", "Tibber", "https://account.tibber.com/", "Utilities"),
+            rule("altibox", ".*\\baltibox\\b.*|.*\\blyse\\b.*altibox.*", "Altibox", "https://www.altibox.no/privat/kundeservice/", "Telecom"),
 
-            rule("microsoft_365", ".*\\bmicrosoft\\b.*(365|office).*|.*\\boffice\\s*365\\b.*|.*\\boffice365\\b.*", "Microsoft 365", "https://account.microsoft.com/services/", "Productivity"),
-            rule("adobe", ".*\\badobe\\b.*(creative\\s*cloud|cc|acrobat)?.*", "Adobe", "https://account.adobe.com/plans", "Productivity"),
-            rule("dropbox", ".*\\bdropbox\\b.*", "Dropbox", "https://www.dropbox.com/account/billing", "Cloud"),
-            rule("github", ".*\\bgithub\\b.*", "GitHub", "https://github.com/settings/billing", "Developer"),
-            rule("notion", ".*\\bnotion\\b.*", "Notion", "https://www.notion.so/my-account", "Productivity"),
-            rule("slack", ".*\\bslack\\b.*", "Slack", "https://my.slack.com/account/billing", "Productivity"),
-            rule("zoom", ".*\\bzoom\\b.*", "Zoom", "https://zoom.us/billing", "Productivity"),
+            rule("chatgpt", ".*\\bopenai\\b.*|.*\\bchatgpt\\b.*", "ChatGPT", "https://chatgpt.com/", "Utilities"),
+            rule("apple_subscriptions", ".*apple\\.com/bill.*|.*itunes\\.com/bill.*|.*\\bitunes\\b.*|.*\\bicloud\\b.*|.*apple\\s*services.*", "Apple (Abonnement)", "https://apps.apple.com/account/subscriptions", "Utilities"),
+            rule("google_play", ".*google\\s*play.*|.*play\\.google\\.com.*|.*google\\s*payment.*", "Google Play (Abonnement)", "https://play.google.com/store/account/subscriptions", "Utilities"),
+            rule("google_one", ".*\\bgoogle\\s*one\\b.*|.*google.*storage.*", "Google One", "https://one.google.com/", "Utilities"),
+            rule("tibber", ".*\\btibber\\b.*", "Tibber", "https://account.tibber.com/", "Utilities"),
+            rule("microsoft_365", ".*\\bmicrosoft\\b.*(365|office).*|.*\\boffice\\s*365\\b.*|.*\\boffice365\\b.*", "Microsoft 365", "https://account.microsoft.com/services/", "Utilities"),
+            rule("adobe", ".*\\badobe\\b.*(creative\\s*cloud|cc|acrobat)?.*", "Adobe", "https://account.adobe.com/plans", "Utilities"),
+            rule("dropbox", ".*\\bdropbox\\b.*", "Dropbox", "https://www.dropbox.com/account/billing", "Utilities"),
+            rule("github", ".*\\bgithub\\b.*", "GitHub", "https://github.com/settings/billing", "Utilities"),
+            rule("notion", ".*\\bnotion\\b.*", "Notion", "https://www.notion.so/my-account", "Utilities"),
+            rule("slack", ".*\\bslack\\b.*", "Slack", "https://my.slack.com/account/billing", "Utilities"),
+            rule("zoom", ".*\\bzoom\\b.*", "Zoom", "https://zoom.us/billing", "Utilities"),
+            rule("nordvpn", ".*\\bnordvpn\\b.*|.*\\bnord\\s*vpn\\b.*", "NordVPN", "https://my.nordaccount.com/billing/", "Utilities"),
+            rule("surfshark", ".*\\bsurfshark\\b.*", "Surfshark", "https://my.surfshark.com/billing", "Utilities"),
+
+            rule("sats", ".*\\bsats\\b.*", "SATS", "https://www.sats.no/kundeservice/", "Health & Fitness"),
+            rule("fresh_fitness", ".*\\bfresh\\s*fitness\\b.*|.*\\bfreshfitness\\b.*", "Fresh Fitness", "https://freshfitness.no/kundeservice/", "Health & Fitness"),
+            rule("strava", ".*\\bstrava\\b.*", "Strava", "https://www.strava.com/settings/subscription", "Health & Fitness"),
 
             rule("aftenposten", ".*\\baftenposten\\b.*|.*\\bschibsted\\b.*aftenposten.*", "Aftenposten", "https://aftenposten.no/kundeservice", "News"),
             rule("vg_plus", ".*\\bvg\\+\\b.*|.*\\bvgpluss\\b.*|.*\\bverdens\\s*gang\\b.*", "VG+", "https://www.vg.no/kundeservice/", "News"),
@@ -53,20 +62,8 @@ public final class KnownMerchants {
             rule("adresseavisen", ".*\\badresseavisen\\b.*|.*\\badressa\\b.*", "Adresseavisen", "https://www.adressa.no/kundeservice", "News"),
             rule("morgenbladet", ".*\\bmorgenbladet\\b.*", "Morgenbladet", "https://www.morgenbladet.no/kundeservice", "News"),
 
-            rule("sats", ".*\\bsats\\b.*", "SATS", "https://www.sats.no/kundeservice/", "Fitness"),
-            rule("fresh_fitness", ".*\\bfresh\\s*fitness\\b.*|.*\\bfreshfitness\\b.*", "Fresh Fitness", "https://freshfitness.no/kundeservice/", "Fitness"),
-            rule("strava", ".*\\bstrava\\b.*", "Strava", "https://www.strava.com/settings/subscription", "Fitness"),
-
-            rule("storytel", ".*\\bstorytel\\b.*", "Storytel", "https://www.storytel.com/no/no/account", "Entertainment"),
-            rule("bookbeat", ".*\\bbookbeat\\b.*", "BookBeat", "https://www.bookbeat.com/no/account", "Entertainment"),
-            rule("fable", ".*\\bfable\\b.*", "Fable", "https://fable.no/kundeservice", "Entertainment"),
-            rule("audible", ".*\\baudible\\b.*", "Audible", "https://www.audible.com/account/cancel", "Entertainment"),
-
-            rule("nordvpn", ".*\\bnordvpn\\b.*|.*\\bnord\\s*vpn\\b.*", "NordVPN", "https://my.nordaccount.com/billing/", "VPN"),
-            rule("surfshark", ".*\\bsurfshark\\b.*", "Surfshark", "https://my.surfshark.com/billing", "VPN"),
-
-            rule("wolt_plus", ".*\\bwolt\\b.*(plus|\\+).*|.*\\bwolt\\+\\b.*", "Wolt+", "https://wolt.com/", "Food"),
-            rule("foodora_plus", ".*\\bfoodora\\b.*(plus|\\+).*", "foodora plus", "https://www.foodora.no/", "Food")
+            rule("wolt_plus", ".*\\bwolt\\b.*(plus|\\+).*|.*\\bwolt\\+\\b.*", "Wolt+", "https://wolt.com/", "Shopping & Food"),
+            rule("foodora_plus", ".*\\bfoodora\\b.*(plus|\\+).*", "foodora plus", "https://www.foodora.no/", "Shopping & Food")
     );
 
     private KnownMerchants() {}
